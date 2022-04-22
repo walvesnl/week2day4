6fe8fbfa36189ded7446d14dbb1045dee6ba93ff
w.alvesnl <w.alvesnl@gmail.com>
2022-04-22 11:26:13 +0200

    few styling changes

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div>
-      <h2 style={{ border: "4px solid black" }}>
+      <h2>
         Hey! This is the <b>about</b> page!
       </h2>
       <p>About what? I don't know!</p>
